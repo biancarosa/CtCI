8590957e0403a7c5a4f689868428eec68dd8e9b9
biancarosa <bmendonca@stone.com.br>
2019-11-02 10:22:52 -0300

    add comment

--- arraylist.py
+++ arraylist.py
@@ -4,10 +4,11 @@ class ArrayList():
 
     def __init__(self):
         self.current_idx = 0
-        self.array_list = array.array('i')
+        self.array_list = array.array('i', 2 )
 
     def append(self, value):
         print(len(self.array_list))
+        # how the fuck do i double this size? should i really be doing this?
         self.array_list[self.current_idx] = value
         self.current_idx += 1
 
