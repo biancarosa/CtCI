c01c46e998eb6d143286ff53634d6f381be67205
Bianca Rosa <me@biancarosa.com.br>
2020-08-29 13:44:03 -0300

    Remove dups

---
+++ chapter-2/implementations/remove_dups.py
@@ -0,0 +1,55 @@
+class LinkedList():
+
+    def __init__(self, node=None):
+        self.head = node
+
+    def append_to_tail(self, data):
+        self.head.append_to_tail(data)
+
+
+class Node():
+
+    def __init__(self, data):
+        self.next = None
+        self.data = data
+
+    def append_to_tail(self, data):
+        next = Node(data)
+        node = self
+        while (node.next):
+            node = node.next
+        node.next = next
+
+
+def remove_dups(ll):
+    count = {}
+    node = ll.head
+    last_node = None
+    while (node):
+        if count.get(node.data):
+            ln.next = node.next
+        else:
+            ln = node
+        count[node.data] = True
+        node = node.next
+
+
+def main():
+    first_node = Node(1)
+    ll = LinkedList(first_node)
+    ll.append_to_tail(2)
+    ll.append_to_tail(3)
+    ll.append_to_tail(2)
+    ll.append_to_tail(3)
+    ll.append_to_tail(3)
+    ll.append_to_tail(3)
+    ll.append_to_tail(3)
+    remove_dups(ll)
+    node = ll.head
+    while (node):
+        print(node.data)
+        node = node.next
+
+
+if __name__ == '__main__':
+    main()
